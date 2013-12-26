4ab64ccd6cef9fccb75798e3087ed2b1e4a3ea22
jetsekoopmans <jetsekoopmans@hotmail.com>
2013-12-26 15:22:14 +0100

    Created *

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PancakeCms::Application.config.secret_key_base = '590a6fc6092e4e527ea7017c8951a6618ef9fbc1127819fcaedbbcb4fb4593c0ea2ca16d5ee301ef731c1811429a35876dbbeb473b751903bee0e861cfd47532'
