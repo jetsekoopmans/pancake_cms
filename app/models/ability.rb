4d6030c6ce3b76bc810629e932b85c44bdf13c92
jetsekoopmans <jetsekoopmans@hotmail.com>
2013-12-26 17:46:17 +0100

    Added superadmin

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user
+      can :access, :rails_admin       # only allow admin users to access Rails Admin
+      can :dashboard                  # allow access to dashboard
+      if user.role == "superadmin"
+        can :manage, :all             # allow superadmins to do anything
+      elsif 
+        # DO NOTHING
+      end
+    end
+  end
+end
