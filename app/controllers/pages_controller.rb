37f468207703f679edf53b1058294776807e8789
aijkoopmans <arend3571@hotmail.com>
2013-12-28 00:02:19 +0100

    function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,12 @@
 class PagesController < ApplicationController
   def index
   	@pages = Pages.all(:order => 'position')
-
   	
-
   	@page = Pages.find_by(url: params[:url])
-
-
-
-  	@title = @page.title
-  	@meta_description = @page.meta_description
-  	@meta_keywords = @page.meta_keywords
-  	@template = 'templates/' + @page.template
-  	@content = ERB.new(@page.content).result.html_safe
+  		@title = @page.title
+  		@meta_description = @page.meta_description
+  		@meta_keywords = @page.meta_keywords
+  		@template = 'templates/' + @page.template
+  		@contents = Content.where(pages_id: @page.id)
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  	def get_content(x)
+    	if @contents[x].present?
+	   		return @contents[x].content.html_safe
+		else
+			return ""
+		end
+  	end
 end
