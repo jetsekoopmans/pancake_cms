77d0cbc8366e9b0b9fb45396277c2257c6080c70
aijkoopmans <arend3571@hotmail.com>
2013-12-27 22:16:40 +0100

    asd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 class PagesController < ApplicationController
   def index
-  	#@page  = Pages.find_by(title: params[:id])
-  	#@page = Pages.find(params[:id])
+  	@pages = Pages.all(:order => 'position')
+
+  	
+
   	@page = Pages.find_by(url: params[:url])
+
+
+
   	@title = @page.title
-  	@content = ERB.new(Pages.first.content).result.html_safe
+  	@meta_description = @page.meta_description
+  	@meta_keywords = @page.meta_keywords
   	@template = 'templates/' + @page.template
+  	@content = ERB.new(@page.content).result.html_safe
   end
 end
