a985d9842d9b54010d19b8a93fdada40bf724877
jetsekoopmans <jetsekoopmans@hotmail.com>
2013-12-27 22:11:05 +0100

    added content

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227170711) do
+ActiveRecord::Schema.define(version: 20131227205955) do
+
+  create_table "contents", force: true do |t|
+    t.string   "content"
+    t.integer  "pages_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pages", force: true do |t|
     t.string   "title"
@@ -21,7 +28,7 @@ ActiveRecord::Schema.define(version: 20131227170711) do
     t.string   "menu"
     t.integer  "position",         default: 0
     t.string   "template",         default: "default"
-    t.text     "content"
+    t.text     "contents"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
