55df56ebd65399062eff9cc68f08da02845c3a17
aijkoopmans <arend3571@hotmail.com>
2013-12-28 11:56:05 +0100

    asd

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,8 +6,8 @@ RailsAdmin.config do |config|
   config.model Content do
     edit do
       # For RailsAdmin >= 0.5.0
-      field :content, :ck_editor
       field :pages
+      field :content, :ck_editor
       # For RailsAdmin < 0.5.0
       # field :description do
       #   ckeditor true
