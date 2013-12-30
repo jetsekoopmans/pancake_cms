e01e93bde527ee03da4ad721881d02fb185a0190
aijkoopmans <arend3571@hotmail.com>
2013-12-30 12:25:08 +0100

    n

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
-  	@pages = Pages.where(ancestry: nil && '0' ).order(position: :asc)
+  	@pages = Pages.where(ancestry: nil ).order(position: :asc)
+    #@pages = Pages.where('ancestry=? OR ancestry=?', '0', nil)
     #@pages = Pages.all.order(position: :desc)
   	if @page = Pages.find_by(url: params[:url])
     elsif @page = Pages.find_by(url: 'home')
